1b519e5addae8107715bca8acefc0d03052eb7c6
Olotu Ude Ebi Nan <ebinan10@gmail.com>
2023-09-19 09:02:16 +0100

    update

--- src/component/service/Service.jsx
+++ src/component/service/Service.jsx
@@ -9,26 +9,26 @@ const Service = () => {
         <div className="w-full bg-[#8c92a9] flex flex-col justify-center items-center">
        <div className=" w-[80%] flex flex-col justify-center items-center my-[6vh] gap-3 " >
        <p className='text-justify text-base-100 md:text-2xl text-2xl font-bold mt-4'> At Nuvie dry Wall and Installations, we offer services not limited to the following </p> <br />
-         <p className='text-justify text-base-100 md:text-xl text-md'> 1. Ceiling Installation : For your different kinds of ceiling applications, including but not limited to Plasterboard,
+         <p className='text-justify text-base-100 md:text-xl text-sm'> 1. Ceiling Installation : For your different kinds of ceiling applications, including but not limited to Plasterboard,
            Insitu POP Ceiling, or the Acoustic T-Grid Systems.
           These are fast and lightweight materials and metal frame construction that can be utilized in different
            areas of a building by using a range of different tiles including metal and mineral fibre which come in 
            many different finishes and colors. Specialist tiles and plasterboard can 
           increase fire resistance, enhance sound absorption and even be used in areas of high humidity. <br />
          </p> 
-         <p className='text-justify text-base-100 md:text-xl text-md'>
+         <p className='text-justify text-base-100 md:text-xl text-sm'>
          2.Framed Partitioning :  Partition walls are usually not load-bearing.
           The use of light weight internal partitions allows flexibility to the client and architect when designing the 
           internal layout of a building. We prioritize on Partition walls constructed of many materials, such as Plasterboards, fibre cement boards, 
           and Magnesium Oxide “MgO” boards. The different systems available can provide the fire, moisture resistant, and acoustic ratings that are required, 
           whilst allowing freedom of decorative design. <br /></p>
-          <p className='text-justify text-base-100 md:text-xl text-md'>
+          <p className='text-justify text-base-100 md:text-xl text-sm'>
           3.Procurement and Supplies : Whether you're simply looking to insulate that cavity, clad the wall, cover up visible ducts or you're working on a conversion project, here at 
           Nuvie dry wall Enterprise and installation we can supply you with everything you need. We have all the options currently available, but if there is nothing off 
           the shelf to serve your purpose, we can work with our over sea suppliers to develop a bespoke solution.Give us your site location and material
           bill of quantity and our logistics team there in a considerable time, no matter your location gice ue a call. <br />
           </p>
-          <p className='text-justify text-base-100 md:text-xl text-md'>
+          <p className='text-justify text-base-100 md:text-xl text-sm'>
           4. Finishing : For the beautiful finishing of your homes and offices contact us and you would be glad you did.
 
 
