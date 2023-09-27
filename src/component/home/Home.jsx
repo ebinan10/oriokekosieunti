ea707c7bda9019c3ca80a5380f24c77f61e2a16f
Olotu Ude Ebi Nan <ebinan10@gmail.com>
2023-09-27 12:33:30 +0100

    update to home page background

--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -1,34 +1,29 @@
 import React from 'react'
 import companyLogo from '../images/companyLogo.jpg'
-
+import './home.css'
+import { Link } from 'react-router-dom'
 const Home = () => {
+  
+  
   return (
-    <div className='md:min-h-[67vh] min-h-[40vh] w-full flex items-center justify-center color-[rgb(12, 48, 80)]'>
+    <div className='md:min-h-[67vh] min-h-[40vh] w-full flex items-center justify-center '>
         <div className="h-full w-full flex flex-col items-center justify-center">
             <div className="h-full w-full flex items-center justify-center ">
-                {/* <div className="h-full w-full">
-                  <img className='h-[75vh] w-full' src="https://img.staticmb.com/mbcontent/images/uploads/2023/1/Low-Cost-Simple-POP-Design_1673522026025.jpg" alt="" />
-                </div> */}
-                {/* <div className="h-full w-full">
-                  <img className='h-[75vh] w-full' src="https://i0.wp.com/cocolapinedesign.com/wp-content/uploads/Brown-kitchen-cabinets-in-a-spacious-turn-of-the-century-home14.jpg?resize=1024%2C768&ssl=1" alt="" />
-                </div> */}
-                {/* <div className="h-full w-full">
-                  <img className='h-[75vh] w-full' src="https://www.decorpot.com/images/1449605336lighting-and-false-ceiling-ideas-for-your-living-room%20(1).jpg" alt="" />
-                </div> */}
-                <div className="md:h-[85vh] h-[55vh] w-full flex items-center justify-center" style={{backgroundImage: `url("https://static.wixstatic.com/media/e5590e78cb3c4a4aa2bd66fce4787a0c.jpg/v1/fill/w_1349,h_528,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/e5590e78cb3c4a4aa2bd66fce4787a0c.jpg")`,
-                  backgroundSize: 'cover', backgroundPosition:'center'}}>
-                  <div className="h-1/5 md:ml-[35vh] ml-[20vh]">
-                  <img src={companyLogo} className=' h-[14vh] w-[10vw]' alt="" />
+               
+                <div id='bg' className="md:h-[85vh] h-[55vh] w-full flex items-center justify-center" >
+                  <div className="md:h-3/5 h-2/5 w-[24vw] flex flex-col gap-[7vh] items-center justify-center "> 
+                  {/* <img src={companyLogo} className=' bg-opacity-100  h-[14vh] w-full' alt="" /> */}
+                  <div className="w-full "><h1 className='text-[brown] md:text-xl text-md '>NUVIE <br /> DRY WALL <br /> ENTREPRISE</h1></div>
+                   <div className="w-full md:w-3/5 bg-opacity-60 h-[5vh] flex items-center justify-center rounded-xl bg-base-100"><Link to='/contact' className='w-full'> Welcome</Link>
+                  </div> 
                   </div>
                   
-                  {/* <img className='md:h-[85vh] h-[55vh] w-full' src="https://static.wixstatic.com/media/e5590e78cb3c4a4aa2bd66fce4787a0c.jpg/v1/fill/w_1349,h_528,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/e5590e78cb3c4a4aa2bd66fce4787a0c.jpg" alt="" /> */}
+                 
                 </div>
-                {/* <div className="h-full w-full">
-                  <img className='h-[75vh] w-full' src="https://www.home-designing.com/wp-content/uploads/2014/02/24-White-yellow-kitchen.jpg" alt="" />
-                </div> */}
+                
             </div>
             <div className="md:w-3/4 w-full md:m-[50px] py-[20px] text-[grey] md:bg-[white] bg-[#8c92a9] flex justify-center items-center"><div className="md:w-full w-[85%]">
-              <h1 className='md:text-2xl text-lg tracking-wide md:text-[#8c92ac] text-[white] my-3' >Welcome to Nuvie Dry Wall Enterprise.</h1>
+              <h1 className='md:text-2xl text-lg tracking-wide md:text-[#8c92ac] text-[white] my-3' >Welcome to Nieve Dry Walls  Ltd.</h1>
               <h1 className='text-justify md:text-xl text-md md:mb-0 mb-3 leading-loose md:text-[#8c92ac] text-[white]'>Nuvie Drywall & Installation Enterprise, situated at Lagos, is a quality brand behind high 
               ceiling and drywall partition products, from wholesale supply to expert installation.  Our products are of affordable prices, aesthetic values, energy efficiency, 
               fire & moisture resistance, fast installation time, and low maintenance cost.<br />As the need for distinctive and extraordinary interior systems are ever growing, NDIE is constantly on the lookout for new products, 
