e8bbfcbea8e4a03decd1516ba7bb8abb01dc59a8
Olotu Ude Ebi Nan <ebinan10@gmail.com>
2023-09-27 19:47:41 +0100

    final

--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -11,15 +11,14 @@ const Home = () => {
             <div className="h-full w-full flex items-center justify-center ">
                
                 <div id='bg' className="md:h-[85vh] h-[55vh] w-full flex items-center justify-center" >
-                  <div className="h-3/5 w-[24vw] flex flex-col gap-[7vh] items-center justify-center "> 
+                <div className="md:h-3/5 h-3/5 md:w-1/4 w-1/2  flex flex-col gap-[7vh] items-center justify-center "> 
                   {/* <img src={companyLogo} className=' bg-opacity-100  h-[14vh] w-full' alt="" /> */}
-                  <div className="w-full flex flex-col gap-2 font-bold">
-                    <h1 className='text-[brown] md:text-3xl text-md '>NUVIE</h1>
-                     <h1 className='text-[brown] md:text-3xl text-md '> DRYWALL</h1>
-                     <h1 className='text-[brown] md:text-3xl text-md '>&</h1>
-                     <h1 className='text-[brown] md:text-3xl text-md '>INSTALLATION</h1> 
-                     <h1 className='text-[brown] md:text-5xl text-xl '> ENTREPRISE</h1></div>
-                   <div className=" md:w-[20vw] w-[30vw] bg-opacity-60 h-[5vh] flex items-center justify-center rounded-xl bg-base-100"><Link to='/contact' className='w-full'> Welcome</Link>
+                  <div className="w-full h-full rounded flex flex-col justify-center items-center gap-2 font-bold text-center bg-opacity-40 bg-[black]">
+                    <h1 className='text-[white] md:text-3xl text-md text-center '>NUVIE</h1>
+                     <h1 className='text-center text-[white] md:text-3xl text-md '> DRYWALL</h1>
+                     <h1 className='text-[white] md:text-3xl text-md '>&</h1>
+                     <h1 className='text-[white] md:text-3xl text-md '>INSTALLATION</h1> <h1 className='text-[white] md:text-5xl text-xl '> ENTREPRISE</h1></div>
+                   <div className="  md:w-[20vw] w-[30vw] bg-opacity-60 h-[5vh] flex items-center justify-center rounded-xl bg-base-100"><Link to='/contact' className='w-full'> Welcome</Link>
                   </div> 
                   </div>
                   
