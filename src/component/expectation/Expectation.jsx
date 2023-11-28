b55409f4591df2afb4daba6da5b34189a5aad738
Olotu Ude Ebi Nan <ebinan10@gmail.com>
2023-11-28 23:45:52 +0100

    adjustment

--- src/component/expectation/Expectation.jsx
+++ src/component/expectation/Expectation.jsx
@@ -16,8 +16,8 @@ const Expectation = () => {
         <h1 className='w-full flex justify-center md:justify-end items-center md:items-end text-[white] text-sm md:text-md'>EXPECTATION</h1>
         <img src={logo} alt="" className='w-1/3 h-[12vh] my-[3vh] md:w-[40vw] md:h-[40vh]' />
           </div>
-          <div className="w-full h-[27vh] md:h-[50vh] bg-[black] max-w-[1024px] flex  justify-center items-center">
-               <h1 className=' text-[white] text-sm md:text-lg text-justify'>
+          <div className="w-full h-[32vh] md:h-[50vh] bg-[black] max-w-[1024px] flex  justify-center items-center">
+               <h1 className=' text-[white] text-sm md:text-md md:pb-0 pb-3 text-justify'>
                   What are you believing the Lord for, <br /> is it Fruit of the womb ? <br />
                   Marriage ? <br /> Job ? <br /> Business Breakthrough ? <br /> Healing? <br />
                   whatever the challenge is just know that <br />
