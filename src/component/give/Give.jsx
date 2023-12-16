7c9ba0076ff1029b6af0f796e53386705a6c58f2
Olotu Ude Ebi Nan <ebinan10@gmail.com>
2023-12-16 11:53:34 +0100

    adjustments for give page

--- src/component/give/Give.jsx
+++ src/component/give/Give.jsx
@@ -12,8 +12,8 @@ const Give = () => {
     referrerpolicy="no-referrer-when-downgrade" className='flex h-full justify-center md:text-2xl items-center text-[white]'>
       <h1>Give Online</h1></iframe>
           </div>
-        <div className="md:full w-[100%] bg-[black] md:min-h-1/2 min-h-[55vh] p-[7vh] md:pb-0 pb-[5vh] flex md:flex-row flex-col justify-center items-center">
-        <div className="md:w-1/3 w-full md:pl-[10vw] md:mt-[5vh] p-3 h-full flex justify-center items-center">
+        <div className="md:full w-[100%] bg-[black] md:min-h-1/2 min-h-[55vh]  md:pb-0 pb-[5vh] flex md:flex-row flex-col justify-center items-center">
+        <div className="md:w-1/3 w-full md:pl-[10vw] md:mt-[5vh]  h-full flex justify-center items-center">
             <img src={kosunti} alt="" className='w-[37vw] md:h-1/2 h-[14vh]'/>
         </div>
              <div className="md:w-2/3 w-full md:h-[90%] h-[35vh] flex flex-col justify-center item-center">
@@ -26,7 +26,7 @@ const Give = () => {
                     or you can ask any of the servant of God on the mountain for the account details of which you can sow your seed, you can also 
                     give your resources directly by coming with it to the mountain.<br/> 
                    </h1>
-                    <h1 className='w-full text-base-100 md:text-md mt-3 flex justify-center items-center text-sm  text-center'>
+                    <h1 className='w-full text-base-100 md:text-md md:pb-0 pb-7 mt-3 flex justify-center items-center text-sm  text-center'>
                     Mobile: +234-8038074339 
                     <br />
                     Phone: +234-9155267388
